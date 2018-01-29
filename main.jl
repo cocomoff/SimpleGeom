3670c06dbf55559c453e61ae89d5bd4bce1e36a0
taki@macmini <taki0313@gmail.com>
2018-01-29 22:30:12 +0900

    Chapter 2

--- main.jl
+++ main.jl
@@ -7,3 +7,8 @@ l1 = frompoints(0., 1., 6., 4.)
 l2 = frompoints(3., 6., 5., 0.)
 p = getintersect(l1, l2)
 println(p)
+
+
+l = frompoints(-2., 5., 3., -4.)
+ls = LineSegment(-1., -1., 2., 3.)
+println(isintersects(l, ls))
--- simplegeom.jl
+++ simplegeom.jl
@@ -2,7 +2,7 @@ module SimpleGeom
 
 # data structures
 
-export Point, Line
+export Point, Line, LineSegment
 
 struct Point
     # represent a point (x, y)
@@ -17,8 +17,15 @@ struct Line
     c::Float64
 end
 
+struct LineSegment
+    x1::Float64
+    y1::Float64
+    x2::Float64
+    y2::Float64
+end
+
 # functions
-export frompoints, getintersect
+export frompoints, getintersect, isintersects, toline
 
 frompoints(p1::Point, p2::Point) = frompoints(p1.x, p1.y, p2.x, p2.y)
 function frompoints(x1::Float64, y1::Float64, x2::Float64, y2::Float64)
@@ -36,5 +43,16 @@ function getintersect(l1::Line, l2::Line)
     y = (l2.a * l1.c - l1.a * l2.c) / d
     return (x, y)
 end
+
+toline(l::LineSegment) = frompoints(l.x1, l.y1, l.x2, l.y2)
+
+isintersects(l::Line, ls::LineSegment) = isintersects(ls, l)
+function isintersects(ls::LineSegment, l::Line)
+    t1 = l.a * ls.x1 + l.b * ls.y1 + l.c
+    t2 = l.a * ls.x2 + l.b * ls.y2 + l.c
+    # return (t1 <= 0 && t2 >= 0) || (t1 >= 0 && t2 <= 0)
+    return t1 * t2 <= 0
+end
+
 
 end
